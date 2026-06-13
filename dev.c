183f67058b7e0ed6ccac9469d208dd09b8123be9
GilbertoDev443 <gilbertocti22@gmail.com>
2026-06-13 15:32:03 -0300

    Adiciona funcao de ler arquivo

--- dev.c
+++ dev.c
@@ -1,12 +1,59 @@
 #include <stdio.h>
-
-typedef struct{
+#include <stdlib.h>
+#include <string.h>
+typedef struct selecao{
     char nome[50];
     int pontuacao;
     int gols;
 }selecao;
 
 selecao copa[48];
+
+
+void lerNomeselecao(){
+  /**
+   * a primeira comparacao do if "if(arq==NULL)" null e para caso o ponteiro nao apontar para canto nenhum, ele fica com null na memoria ai a funcao ja encerra
+   * o return e para encerrar a funcao
+   * while e para ler as selecoes e armazenar ela no vetor copa
+   *  */  
+FILE *arq;
+arq = fopen("selecoes.txt","r");
+if(arq==NULL){
+    printf("Problema ao abrir o arquivo!");
+    return ;
+}else{
+    int i=0;
+    while(fscanf(arq, "%s", copa[i].nome) != EOF ){
+        i++;
+    }
+}
+fclose(arq);
+return ;
+}
+/**void lerPlacar(){
+FILE *arq;
+arq = fopen("jogos.txt","r");
+if(arq==NULL){
+    printf("Problema ao abrir o arquivo!");
+    return ;
+}else{
+    int i=0;
+    selecao nomeA[50],nomeB[50];
+    while(fscanf(arq, "%s %d %d %s", copa[i].nome) != EOF ){
+        i++;
+    }
+}
+fclose(arq);
+return ;
+}
+*/
+
+
+
+
+
+
+
 
 int main(){
 
